00c36b4470c3968cd2b0e42da42a5ba1f9f53cd1
yixianloww <97507079+yixianloww@users.noreply.github.com>
2022-04-15 21:41:34 +0800

    numbers and assignments (#1)
    * numbers and assignments
    
    * Create go.mod
    
    * renamed mean.go to main.go
    
    * updated workflows
    
    * Update go.yml
    
    * removed go.yml
    
    * Create hello-world.yml
    
    * Update hello-world.yml
    
    * Update hello-world.yml
    
    * Update lint.yaml

---
+++ mean/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x, y := 1.0, 2.0
+	fmt.Printf("x=%v, type of %T\n", x, x)
+	fmt.Printf("y=%v, type of %T\n", y, y)
+
+	// this results in a float - the resulting type will follow x and y, not 2
+	mean := (x + y) / 2
+	fmt.Printf("result: %v, type of %T\n", mean, mean)
+}
